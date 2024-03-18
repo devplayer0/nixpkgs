122b65d4197bbcbb8308828313e21cdc0137eb7e
Jack O'Sullivan <jackos1998@gmail.com>
2024-03-18 16:25:10 +0000

    nixos/systemd/networkd: Add missing network section options

--- nixos/modules/system/boot/networkd.nix
+++ nixos/modules/system/boot/networkd.nix
@@ -224,6 +224,7 @@ let
       sectionVLAN = checkUnitConfig "VLAN" [
         (assertOnlyFields [
           "Id"
+          "Protocol"
           "GVRP"
           "MVRP"
           "LooseBinding"
@@ -231,6 +232,7 @@ let
         ])
         (assertInt "Id")
         (assertRange "Id" 0 4094)
+        (assertValueOneOf "Protocol" ["802.1q" "802.1ad"])
         (assertValueOneOf "GVRP" boolValues)
         (assertValueOneOf "MVRP" boolValues)
         (assertValueOneOf "LooseBinding" boolValues)
@@ -685,6 +687,9 @@ let
           "Xfrm"
           "KeepConfiguration"
           "BatmanAdvanced"
+          "IPoIB"
+          "L2TP"
+          "MACVTAP"
         ])
         # Note: For DHCP the values both, none, v4, v6 are deprecated
         (assertValueOneOf "DHCP" (boolValues ++ ["ipv4" "ipv6"]))
@@ -946,9 +951,13 @@ let
 
       sectionIPv6AcceptRA = checkUnitConfig "IPv6AcceptRA" [
         (assertOnlyFields [
+          "Token"
           "UseDNS"
           "UseDomains"
           "RouteTable"
+          "UseMTU"
+          "UseGateway"
+          "UseRoutePrefix"
           "UseAutonomousPrefix"
           "UseOnLinkPrefix"
           "RouterDenyList"
@@ -967,6 +976,9 @@ let
         (assertValueOneOf "UseDNS" boolValues)
         (assertValueOneOf "UseDomains" (boolValues ++ ["route"]))
         (assertRange "RouteTable" 0 4294967295)
+        (assertValueOneOf "UseMTU" boolValues)
+        (assertValueOneOf "UseGateway" boolValues)
+        (assertValueOneOf "UseRoutePrefix" boolValues)
         (assertValueOneOf "UseAutonomousPrefix" boolValues)
         (assertValueOneOf "UseOnLinkPrefix" boolValues)
         (assertValueOneOf "DHCPv6Client" (boolValues ++ ["always"]))
